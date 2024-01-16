8cefe8e726feacdd76e69d669eb21cc6c8b37d23
João Henrique Rodrigues <115806935+JoaoHr1@users.noreply.github.com>
2024-01-16 09:26:55 +0000

    feat: imported navbar and footer to app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SobreComponent } from './pages/sobre/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sobre', component: SobreComponent }, // /sobre page route
+];
