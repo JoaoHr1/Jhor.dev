fd4c65452881464f41dafca28d95d2e36ee1a988
João Henrique Rodrigues <115806935+JoaoHr1@users.noreply.github.com>
2024-01-17 07:12:01 +0000

    some responsivity updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { SobreComponent } from './pages/sobre/sobre/sobre.component';
+import { PostsComponent } from './pages/posts/posts/posts.component';
 
 export const routes: Routes = [
-  { path: 'sobre', component: SobreComponent }, // /sobre page route
+  { path: '', component: PostsComponent },
+  { path: 'sobre', component: SobreComponent }, 
+  { path: '**', redirectTo: '', pathMatch: 'full' } 
+
 ];
